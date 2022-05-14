36f095656a89858ed1fa7480ee79539147060ffd
MDABDULLAH16 <mdabdullah161036@gmail.com>
2022-05-15 01:02:24 +0600

    delete data from db

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ async function run() {
             res.send(result);
         });
 
-        // app.delete('/food/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await foodCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/food/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await foodCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
